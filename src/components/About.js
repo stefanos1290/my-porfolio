70b556ace0257241f72c0c371b14f9251129a1ac
str1290 <stefanostrr@hotmail.com>
2020-06-07 23:46:54 +0200

    complited about

--- src/components/About.js
+++ src/components/About.js
@@ -38,8 +38,58 @@ const About = () => {
   return (
     <div className="about-container">
       <div className="text-container">
+        <div className="moblieResposiveNumbers">
+          <span className="moblieResposiveNum">01</span>
+          <span className="moblieResposiveNum">02</span>
+          <span className="moblieResposiveNum">03</span>
+          <span className="moblieResposiveNum">04</span>
+          <span className="moblieResposiveNum">05</span>
+          <span className="moblieResposiveNum">06</span>
+          <span className="moblieResposiveNum">07</span>
+          <span className="moblieResposiveNum">08</span>
+          <span className="moblieResposiveNum">09</span>
+          <span className="moblieResposiveNum">10</span>
+          <span className="moblieResposiveNum">11</span>
+          <span className="moblieResposiveNum">12</span>
+          <span className="moblieResposiveNum">13</span>
+          <span className="moblieResposiveNum">14</span>
+          <span className="moblieResposiveNum">15</span>
+          <span className="moblieResposiveNum">16</span>
+          <span className="moblieResposiveNum" id="n17">
+            17
+          </span>
+          <span className="moblieResposiveNum" id="n18">
+            18
+          </span>
+          <span className="moblieResposiveNum" id="n19">
+            19
+          </span>
+          <span className="moblieResposiveNum" id="n20">
+            20
+          </span>
+          <span className="moblieResposiveNum" id="n21">
+            21
+          </span>
+          <span className="moblieResposiveNum" id="n22">
+            22
+          </span>
+          <span className="moblieResposiveNum" id="n23">
+            23
+          </span>
+          <span className="moblieResposiveNum" id="n24">
+            24
+          </span>
+          <span className="moblieResposiveNum" id="n25">
+            25
+          </span>
+          <span className="moblieResposiveNum" id="n26">
+            26
+          </span>
+          <span className="moblieResposiveNum" id="n27">
+            27
+          </span>
+        </div>
         <div className="classStart">
-          <span className="num">01</span>
           class{" "}
           <span className="nameStartFunction">
             {myName}
@@ -47,30 +97,20 @@ const About = () => {
           </span>
         </div>
         <div>
-          <span className="num">02</span>
           <span className="startingDots">{startingDots}</span>
           <span className="firstComment">{firstComment}</span>
         </div>
         <div>
-          <span id="num3931px" className="num">
-            03
-          </span>
           <span className="startingDots">{startingDots}</span>
           <span className="secondComment">{secondComment}</span>
         </div>
         <div>
-          <span id="num41055px" className="num">
-            04
-          </span>
           <span className="startingDots">{startingDots}</span>
           <span className="constructor">constructor</span>
           <span className="parenthesis"> {parenthesis}</span>
           <span className="openBracket"> {openBracket}</span>
         </div>
         <div>
-          <span id="num51055px" className="num">
-            05
-          </span>
           <span className="startingDots">{startingDots}</span>
           <span className="startingDots">{startingDots}</span>
           <span className="constructorThis">{constructorThis}</span>
@@ -81,9 +121,6 @@ const About = () => {
           <span className="semicolon">;</span>
         </div>
         <div>
-          <span id="num61055px" className="num">
-            06
-          </span>
           <span className="startingDots">{startingDots}</span>
           <span className="startingDots">{startingDots}</span>
           <span className="constructorThis">{constructorThis}</span>
@@ -94,9 +131,6 @@ const About = () => {
           <span className="semicolon">;</span>
         </div>
         <div>
-          <span id="num71055px" className="num">
-            07
-          </span>
           <span className="startingDots">{startingDots}</span>
           <span className="startingDots">{startingDots}</span>
           <span className="constructorThis">{constructorThis}</span>
@@ -107,18 +141,12 @@ const About = () => {
           <span className="semicolon">;</span>
         </div>
         <div>
-          <span id="num81055px" className="num">
-            08
-          </span>
           <span className="startingDots">{startingDots}</span>
           <span className="functionSkills">skills</span>
           <span className="parenthesis">{parenthesis}</span>
           <span className="openBracket"> {openBracket}</span>
         </div>
         <div>
-          <span id="num91055px" className="num">
-            09
-          </span>
           <span className="startingDots">{startingDots}</span>
           <span className="startingDots">{startingDots}</span>
           <span className="return">return</span>
@@ -127,43 +155,27 @@ const About = () => {
             return <span className="mySkills">{x}, </span>;
           })}{" "}
           <br />
-          <span id="num101403px" className="num">
-            10
-          </span>
           <span className="startingDots">{startingDots}</span>
           <span className="startingDots">{startingDots}</span>
           {mySkills2.map((x) => {
             return <span className="mySkills">{x}, </span>;
           })}
-          <span className="num1475px">11</span>
           <span className="startingDots1475px">{startingDots}</span>
           <span className="startingDots825px">{startingDots}</span>
           <span className="startingDots825px">{startingDots}</span>
           <span className="closeSquareBracket">{closeSquareBracket}</span>
         </div>
         <div>
-          <span id="first11" className="num">
-            11
-          </span>
-          <span className="num121475px">12</span>
           <span className="startingDots">{startingDots}</span>
           <span className="closeBracket">{closeBracket}</span>
         </div>
         <div>
-          <span id="first12" className="num">
-            12
-          </span>
-          <span className="num131475px">13</span>
           <span className="startingDots">{startingDots}</span>
           <span className="exprerience">exprerience</span>
           <span className="parenthesis">{parenthesis}</span>
           <span className="openBracket"> {openBracket}</span>
         </div>
         <div>
-          <span id="first13" className="num">
-            13
-          </span>
-          <span className="num141475px">14</span>
           <span className="startingDots">{startingDots}</span>
           <span className="startingDots">{startingDots}</span>
           <span className="return">return</span>
@@ -177,37 +189,15 @@ const About = () => {
           <span className="semicolon">;</span>
         </div>
         <div>
-          <span id="first14" className="num">
-            14
-          </span>
-          <span className="num151475px">15</span>
           <span className="startingDots">{startingDots}</span>
           <span className="startingDots">{startingDots}</span>
           <span className="closeBracket">{closeBracket}</span>
         </div>
         <div>
-          <span id="first15" className="num">
-            15
-          </span>
-          <span className="num161475px">16</span>
           <span className="startingDots1403">{startingDots}</span>
           <span className="closeBracket">{closeBracket}</span>
         </div>
         <div>
-          <span id="first16" className="num">
-            16
-          </span>
-          <span className="num171475px">17</span>
-          <span className="num181403px">18</span>
-          <span className="num191403px">19</span>
-          <span className="num201055px">20</span>
-          <span className="num21931px">21</span>
-          <span className="num22930px">22</span>
-          <span className="num23896px">23</span>
-          <span className="num24825px">24</span>
-          <span className="num25727px">25</span>
-          <span className="num26720px">26</span>
-          <span className="num27672px">27</span>
           <span className="callFunction">{myName}</span>
           <span className="parenthesis">{parenthesis}</span>
           <span className="semicolon">;</span>
